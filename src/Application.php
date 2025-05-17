a7960c4153211114cb2f7934309a55c786e34089
Dejan <dejan_web@outlook.com>
2025-05-17 12:09:05 +0200

    averageCalculationVehiclePark

--- src/Application.php
+++ src/Application.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace VehiclePark;
+
 use Exception;
 use VehiclePark\Vehicle\Car;
 use VehiclePark\Vehicle\Motobike;
@@ -12,37 +13,46 @@ class Application
      * @return void
      * @throws Exception
      */
-    public static function start()
+    public static function start(): void
     {
         $car = new Car([
-            "id"=>"BG1245",
-            "brand"=>"AUDI",
-            "model"=>"A4",
-            "yearOfManufacture"=>2025,
-            "engine"=>1989,
-            "travelTime"=>220,
-            "distance"=>120,
-            "fuelType"=>"diesel",
-            "numberOfWheels"=>4
+            "id" => "BG1245",
+            "brand" => "AUDI",
+            "model" => "A4",
+            "yearOfManufacture" => 2025,
+            "engine" => 1989,
+            "travelTime" => 220,
+            "distance" => 120,
+            "fuelType" => "diesel",
+            "numberOfWheels" => 4
         ]);
-        var_dump($car);
+
         $motobike = new Motobike([
-            "id"=>"541445",
-            "brand"=>"Yamaha",
-            "model"=>"ff155",
-            "yearOfManufacture"=>2022,
-            "engine"=>750,
-            "fuelType"=>"gasoline",
-            "numberOfWheels"=>2,
-            "travelTime"=>4.5,
-            "distance"=>100
+            "id" => "541445",
+            "brand" => "Yamaha",
+            "model" => "ff155",
+            "yearOfManufacture" => 2022,
+            "engine" => 750,
+            "fuelType" => "gasoline",
+            "numberOfWheels" => 2,
+            "travelTime" => 4.5,
+            "distance" => 100
         ]);
-        var_dump($motobike);
+
         echo $motobike->speedCalculation();
         $vehiclePark01 = new VehiclePark();
-        $vehiclePark01->addVehicle($car);
-        var_dump($vehiclePark01);
-        echo $car->consumedFuelPrice();
+        $vehiclePark01->addVehicle($car)->addVehicle($motobike);
+        ?>
+        <pre>
+            <?php
+            var_dump($vehiclePark01);
+            ?>
+        </pre>
+        <?php
+        echo $car->consumedFuelPrice(). "<br>";
+        echo $motobike->consumedFuelPrice(). "<br>";
+        echo $vehiclePark01->averageFuelPriceCalculation(). "<br>";
+        echo $vehiclePark01->averageSpeedCalculation(). "<br>";
 
     }
 }
--- src/VehiclePark/VehiclePark.php
+++ src/VehiclePark/VehiclePark.php
@@ -7,15 +7,34 @@ use VehiclePark\Vehicle\Vehicle;
 
 class VehiclePark
 {
-protected $vehicles = [];
+    protected array $vehicles = [];
+
     public function getVehicles(): array
     {
         return $this->vehicles;
     }
+
     public function addVehicle(Vehicle $vehicle): VehiclePark
     {
         $this->vehicles[] = $vehicle;
         return $this;
+    }
+
+    public function averageSpeedCalculation(): float|int
+    {
+        $fullSpeed = 0;
+        foreach ($this->vehicles as $vehicle) {
+            $fullSpeed += $vehicle->speedCalculation();
+        }
+        return $fullSpeed / count($this->vehicles);
+    }
+    public function averageFuelPriceCalculation(): float|int
+    {
+        $fullFuelPrice = 0;
+        foreach ($this->vehicles as $vehicle) {
+            $fullFuelPrice += $vehicle->consumedFuelPrice();
+        }
+        return $fullFuelPrice / count($this->vehicles);
     }
     public function __construct()
     {
